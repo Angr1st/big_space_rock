dfc2f40650c6157a2951b99fea467b5985f773e3
Torben Clasen <t.clasen@outlook.com>
2024-03-29 00:32:02 +0100

    Min 29:55

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
-use macroquad::prelude::*;
+use macroquad::{miniquad::window, prelude::*};
+const THICKNESS = 3.0;
+fn window_conf() -> Conf {
+    Conf {
+        window_title: String::from("BIG SPACE ROCKS"),
+        window_width: 1280,
+        window_height: 960,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
 
-#[macroquad::main("BIG SPACE ROCKS")]
+struct State {
+    ship_position: Vec2,
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
+    debug!("Helloaaaa, world!\n");
+
     loop {
         clear_background(BLACK);
-        request_new_screen_size(1280.0, 720.0);
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        let a = Vec2::splat(10.0);
+        let b = Vec2::splat(100.0);
 
-        next_frame().await
+        draw_line_vec2(a, b, THICKNESS, WHITE);
+
+        next_frame().await;
     }
+}
+fn draw_ship(pos: Vec2) {}
+
+fn draw_line_vec2(pos1: Vec2, pos2: Vec2, thickness: f32, color: Color) {
+    draw_line(pos1.x, pos1.y, pos2.x, pos2.y, thickness, color);
 }

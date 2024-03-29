6fbed29769c7f4761c00e18354efa3e202ae3ae6
Torben Clasen <t.clasen@outlook.com>
2024-03-29 19:22:10 +0100

    48:43

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use macroquad::{miniquad::window, prelude::*};
+use macroquad::prelude::*;
 
 const THICKNESS: f32 = 3.0;
+const LINE_COLOR: Color = WHITE;
 
 fn window_conf() -> Conf {
     Conf {
@@ -23,15 +24,31 @@ async fn main() {
     loop {
         clear_background(BLACK);
 
-        let a = Vec2::splat(10.0);
-        let b = Vec2::splat(100.0);
-
-        draw_line_vec2(a, b, THICKNESS, WHITE);
+        let points = [
+            Vec2::new(-0.4, -0.5),
+            Vec2::new(0.0, 0.5),
+            Vec2::new(0.4, -0.5),
+            Vec2::new(0.3, -0.4),
+            Vec2::new(-0.3, -0.4),
+        ];
+        draw_lines(Vec2::splat(40.0), 16.0, &points);
 
         next_frame().await;
     }
 }
-fn draw_ship(pos: Vec2) {}
+
+fn draw_lines(origin: Vec2, scale: f32, points: &[Vec2]) {
+    let apply = |p: Vec2| (p * scale) + origin;
+
+    let length = points.len();
+    for i in 0..=length - 1 {
+        let pos1 = points.get(i).unwrap();
+        let pos2 = points.get(i + 1 % length).unwrap();
+        draw_line_vec2(apply(*pos1), apply(*pos2), THICKNESS, LINE_COLOR);
+    }
+}
+
+//fn draw_ship(pos: Vec2) {}
 
 fn draw_line_vec2(pos1: Vec2, pos2: Vec2, thickness: f32, color: Color) {
     draw_line(pos1.x, pos1.y, pos2.x, pos2.y, thickness, color);
